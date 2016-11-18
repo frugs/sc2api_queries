edf7b45f67f1c3d4ad21dd1bcc86e86b3b0b38ba
Hugo Wainwright <wainwrighthugo@gmail.com>
2016-11-18 14:47:53 +0000

    added license and setup stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sc2api_queries',
+  packages = ['sc2api_queries'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Utility for making queries on the SC2 Game Data API',
+  author = 'Hugo Wainwright',
+  author_email = 'wainwrighthugo@gmail.com',
+  url = 'https://github.com/frugs/sc2api_queries',
+  download_url = 'https://github.com/frugs/sc2api_queries/tarball/0.0.1', # I'll explain this in a second
+  keywords = ['ladder', 'starcraft', 'sc2', 'api', 'query', 'battle', 'net'],
+  classifiers = [],
+)
