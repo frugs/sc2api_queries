41882db271ad070e44af04c93d631addfb6c5e3d
Hugo Wainwright <wainwrighthugo@gmail.com>
2016-11-20 20:10:52 +0000

    exposed accessors to standard api endpoints

--- refresh_cache.py
+++ refresh_cache.py
@@ -5,8 +5,8 @@ _access_token = os.environ["BATTLE_NET_ACCESS_TOKEN"]
 
 
 def refresh_cache(access_token: str):
-    ladder_data = sc2gamedata.download_ladder_data(access_token)
-    sc2gamedata.save_to_cache(ladder_data)
+    game_data = sc2gamedata.get_game_data(access_token)
+    sc2gamedata.save_to_cache(game_data)
 
 if __name__ == '__main__':
     refresh_cache(_access_token)
--- sc2gamedata/__init__.py
+++ sc2gamedata/__init__.py
@@ -1,3 +1,3 @@
 from .gamedata import GameData
-from .download import download_ladder_data
+from .download import get_game_data, get_ladder_data, get_current_season_data, get_league_data
 from .saveload import save_to_cache, load_cache
